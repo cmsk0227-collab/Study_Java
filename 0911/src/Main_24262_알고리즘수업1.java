ff0669bd2216b43c5b9a38bfdb0970d6c45ebbba
이지언 <cmsk0227@gmail.com>
2025-09-11 14:44:34 +0900

    commit

---
+++ 0911/src/Main_24262_알고리즘수업1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main_24262_알고리즘수업1 {
+	public static void main(String[] args) {
+		System.out.println("1"); // 수행 횟수 = 연산 횟수
+		System.out.println("0"); // 0
+	}
+}
---
+++ 0911/src/Main_24264_알고리즘수업3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_24264_알고리즘수업3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n); // 수행 횟수 = 연산 횟수
+		System.out.println("1"); // 0
+	}
+}
