4e3252ccc1cea799d4a6bd8619266a5560e5cd22
이지언 <cmsk0227@gmail.com>
2025-09-12 17:58:46 +0900

    .

---
+++ 0912/src/Main_1463_1로_만들기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_1463_1로_만들기 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+			
+	}
+}
