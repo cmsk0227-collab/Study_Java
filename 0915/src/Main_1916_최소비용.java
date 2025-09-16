6ad53600eb51364becee24d3ea248346b95749d1
이지언 <cmsk0227@gmail.com>
2025-09-17 08:52:04 +0900

    ddd

--- 0915/src/Main_1916_최소비용.java
+++ 0915/src/Main_1916_최소비용.java
@@ -69,10 +69,9 @@ public class Main_1916_최소비용{
 			
 			for(Node next : graph[now_v]) {//인접노드 조회 왜 안돼
 				if(dist[next.v] > dist[now_v]+now_cost) {
-					dist[next.v] = dist[now_v]+now_cost;
+					dist[next.v] = dist[now_v]+next.cost;
 					pq.add(new Node(now_v, now_cost));
 				}
-				
 			}
 			
 		}
