2664805704dc0d78b7ea7e0269dd539c8451b06a
이지언 <cmsk0227@gmail.com>
2025-09-16 17:59:23 +0900

    ,,

--- 0915/src/Main_1916_최소비용.java
+++ 0915/src/Main_1916_최소비용.java
@@ -1,142 +1,79 @@
 import java.util.*;
 import java.io.*;
 
-
-
-// 문제 : Main_1916_최소비용구하기
-// 난이도 : 어려움
-// 메모리 : 50588KB
-// 시간 : 472ms
-// 풀이 아이디어 : 다익스트라  <= 양의 가중치 + 그래프에서의 최단 거리 
-//  
- 
-
-/*
- * 우선순위 큐 동작원리 *
- 1. Node 객체(속성+메서드)를 pq 삽입
- 2. pq 내에서 새로 들어온 원소와 기존 원소 비교
- 3. 비교할 때 compareTo() 호출해 음수 / 0 / 양수를 반환받음
- 	반환값 
- 	음수 -> this가 앞
- 	0 -> 순서 유지
- 	양수 0> this가 뒤
- */
-
-public class Main_1916_최소비용 {
-	static int n,m,start, end ;
-	static int[] dist; 
-	static ArrayList<ArrayList<Node>> graph;//인접리스트 구현
+public class Main_1916_최소비용{
+	static int INF = 1000000;
+	static int n, m, start, end;
+	static ArrayList<Node>[] graph;
+	static int[] dist;
+	static class Node implements Comparable<Node>{
+		int v, cost;
+		
+		Node(int v, int cost) {
+			this.v = v;
+			this.cost = cost;
+		}
+		
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		n = Integer.parseInt(br.readLine()); //노드 - 도시
-		m = Integer.parseInt(br.readLine()); //간선 - 버스
+		n = Integer.parseInt(br.readLine()); //노드 
+		m = Integer.parseInt(br.readLine()); //간선
 		
-//		[이중리스트 구현]
-//		(1) 외부리스트
-		 graph = new ArrayList<>();
-		for(int i = 0; i <= n ; i++) { //노드 번호 1~n번
-//		(2) 내부리스트 
-			graph.add(new ArrayList<>());
+		//그래프 구현
+		graph = new ArrayList[n+1];
+		for(int i = 1; i <= n ; i++) {
+			graph[i] = new ArrayList<>();
 		}
 		
-		//인접한 정점과 가중치 입력
-		for(int i = 0; i < m; i++) {
+		for(int i = 1 ; i <= m ; i++) {
 			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());//탐색정점 - 출발지
-			int v = Integer.parseInt(st.nextToken());//탐색정점과 인접한 노드 - 도착지
-			int w = Integer.parseInt(st.nextToken());//탐색정점과 인접 노드까지의 가중치 - 버스이용금
-			graph.get(u).add(new Node(v, w));//Node 클래스의 객체(속성,메서드)를 u번 리스트에 입력. 
+			int u = Integer.parseInt(st.nextToken());//탐색노드
+			int v = Integer.parseInt(st.nextToken());//인접노드
+			int w = Integer.parseInt(st.nextToken());//탐색 -> 인접 가중치
+			graph[u].add(new Node(v, w));
 		}
 		
-		 st = new StringTokenizer(br.readLine());
-		 start = Integer.parseInt(st.nextToken());//구하고자하는 구간의 시작점	
-		 end = Integer.parseInt(st.nextToken());//구하고자하는 구간의 도착점
+		st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
 		
-		//로직
-		dist = new int[n+1]; // 시작점에서 i까지의 최단거리
-		Arrays.fill(dist, 1000000);//MAX_VALUE보다 10^6 이나 10^7을 써야 오버플로우 안남.
-		daji();//클래스 필드에 있는 메서드는 매게변수가 없어도 순서가 되면 호출. 꼭 인자를 적을 필요는 없음.
-		System.out.println(dist[end]);//출발점부터 end까지의 최단거리 출력
+		//다익스트라
+		dist = new int[n+1]; //도시번호 1~n반
+		Arrays.fill(dist, INF);//최솟값 갱신할 수 있도록 초기화.
 		
-		
+		daji();
+		System.out.println(dist[end]);
 	}
 	
-	
-	/*
-	 * 다익스트라 구현
-	 * 1. 우선순위큐  
-	 * 2. 중복방문을 막기 위한 boolean 배열 또는 if(dist[v] < w)
-	 * 3. 기존 최단거리와 탐색중인 v와 인접한 노드까지 최단거리를 비교해 더 작은 값으로 갱신 
-	 */
-	private static void daji(){
-		PriorityQueue<Node> pq = new PriorityQueue<>();//큐에 현재 담아긴 자료 중 가장 작은 값을 반환.
+	private static void daji() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
 		
-		pq.add(new Node(start, 0));//시작점 - 시작점의 가중치는 항상 0. 
-		dist[start] = 0;//출발점에서 출발점까지의 거리 0
+		//시작점
+		dist[start] = 0;
+		pq.offer(new Node(start, 0));
 		
+		//연산시작!
 		while(!pq.isEmpty()) {
-			Node now = pq.poll();//가중치 기준으로 가장 작은 값을 꺼냄.
-			int v = now.v;
-			int w = now.w;
+			Node now = pq.poll();//현재 탐색노드 { 인접 노드, 가중치 , 비교메서드}
+			int now_v = now.v;// 인접노드
+			int now_cost = now.cost;// 인접노드의 가중치
 			
-			if(dist[v] < w) continue;
-//			dist[] = 바로 직전 노드까지의 최단거리 + 가중치
-//			이 때 가중치인 w가 최단거리도 크다면 무엇을 합하든 항상 이전 최단거리 값보다 크므로 굳이 계산할 필요 x
-//			continue.
-//			
-			for(Node next : graph.get(v)) {
-				if(dist[next.v] > dist[v] + next.w) {
-					dist[next.v] = dist[v] + next.w;
-					pq.add(new Node(next.v, dist[next.v]));
-				}
+			if(dist[now_v] < now_cost) continue; 
+			
+			for(Node next : graph[now_v]) {//인접노드 조회
+				if(dist[next.v] > dist[])
+				
 			}
 			
 		}
 		
 	}
 	
-	//클래서 Node 선언. 그리고 이 노드는 node와 비교하는 기능도 함께 수행한다
-
-//	 인터페이스  Comparable<T> 
-//	  규칙
-//	  디폴트 : 숫자 - 오름차순, 문자 - 사전순(ㄱㄴㄷ,abc) 날짜 - 빠른 날짜부터 정렬
-//	 a.compareTo(b)의 반환값이 음수 -> a < b -> a, b
-//	 a.compareTo(b)의 반환값이 0 -> a == b -> 순서 유지
-//	 a.compareTo(b)의 반환값이 음수 -> a < b -> b, a
-	
-	static class Node implements Comparable<Node>{//노드끼리 비교하도록 인터페이스 구현 -> pq에서 자동정렬
-		//필드
-		int v;//도착 정점
-		int w;//가중치
-		
-		//생성자
-		Node(int v, int w) {//v,w 초기화
-			this.v = v;
-			this.w = w;
-		}
-		
-		//CompareTo 메서드 - 두 Node 객체 비교 시 호출
-		@Override
-		public int compareTo(Node node) {
-			return this.w - node.w;//정렬 기준 -가중치 오름차순
-			
-//			 this.w - node.w 결과가 음수면 → this.w < node.w → this가 앞에 유지됨 (작은 값 우선)
-//			 결과가 양수면 → this.w > node.w → this가 뒤로 감 (큰 값 뒤로)
-//			 따라서 작은 w가 먼저 나오므로 오름차순 정렬
-		}
-		
-		
-	}
-
 }
-
-//클래스 선언 형식
-//    [접근제어자] class 클래스명 [extends 부모클래스] [implements 인터페이스들] {
-// 			필드(멤버 변수)
-//			생성자
-// 			메서드
-// 			내부 클래스(선택)
-//						}
